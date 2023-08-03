3d0fa528d514571143a09fd1f37b5c304d3bdf00
Derick1530 <DerickZIHALIRWA07@gmail.com>
2023-08-03 22:20:31 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,23 @@ export interface User {
   username: string
   roomId: string
 }
+
+export type Message = {
+  type: 'text' | 'notification'
+  data: string
+} & {
+  id: string
+  sender: User
+  createdAt: string
+}
+
+export interface Messages {
+  [key: string]: Message[]
+}
+
+export type MessagePayload = {
+  type: 'text' | 'notification'
+  data: string
+} & {
+  roomId: string
+}
